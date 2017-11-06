58e7026570305cc45e08f2dd2953a442ac1e7b8a
lierica <ericaaa.li@gmail.com>
2017-11-06 15:24:08 -0500

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABlE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  name TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+    id INTEGER PRIMARY KEY,
+    book_id INTEGER,
+    character_id INTEGER
+)
